3642340fe5c49d4923300bd94d45adac07503e1a
ananjaykumar2 <ananjaykumar2@gmail.com>
2021-11-07 11:48:24 +0530

    my first commit

---
+++ calculate.js
@@ -0,0 +1,27 @@
+
+function calculate(){
+    let amount = document.getElementById("bill").value;
+    let service = document.getElementById("service").value;
+    let people = document.getElementById("people").value;
+
+    if(amount==="" || service===0){
+        alert("Enter value");
+        return;
+    }
+    if(people===""|| people<=0){
+    people = 1;
+    document.getElementById("each").style.display = "none";
+    }
+    else{
+        document.getElementById("each").style.display = "block";
+    }
+
+    let total = (amount*service)/people;
+    total = Math.round(total*100)/100;
+    total = total.toFixed(2);// fixed two digits after decimal
+    console.log(total);
+    document.getElementById("totaltip").style.display = "block";
+    document.getElementById("tip").innerHTML = total;
+    
+}
+
